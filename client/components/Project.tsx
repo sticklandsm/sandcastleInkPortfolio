28d3f49ada44a79ebbc91cb54bd860c92d93a6a2
Sean Stickland <sticklandsm@gmail.com>
2023-05-24 12:57:52 +1200

    Fixed the contact page on mobile

--- client/components/Project.tsx
+++ client/components/Project.tsx
@@ -10,8 +10,8 @@ export default function Project(props: Props) {
     props.project
   return (
     <div
-      style={{ height: 700 }}
-      className=" flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-5xl mx-auto border border-white bg-white"
+      // style={{ height: 900 }}
+      className=" flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl md:h-[700px] shadow-lg p-3 max-w-xs md:max-w-5xl mx-auto border border-white bg-white"
     >
       <div className="w-full md:w-1/3  bg-white grid place-items-center">
         <Carousel images={imagesURL} />
@@ -23,17 +23,19 @@ export default function Project(props: Props) {
         </h3>
         <a
           className="md:text-base underline text-blue-600 hover:text-blue-800 visited:text-purple-600 text-sm"
+          target="_blank"
+          rel="noreferrer"
           href={linkToProject}
         >
           Check it out here!
         </a>
         <p className="md:text-base text-gray-500 text-sm">{details}</p>
-        <ul className=" text-sm relative flex justify-center items-center flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white align-middle">
+        <ul className=" text-sm relative flex justify-center items-center flex-wrap flex-none  md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white align-middle">
           {thingsUsed.map((thingUsed, index) => {
             return (
               <li
                 key={index}
-                className="bg-gray-200 px-3 py-1 rounded-full text-gray-800 hidden md:block text-center "
+                className="bg-gray-200 px-3 py-1 rounded-full text-gray-800  md:block text-center "
               >
                 {thingUsed}
               </li>
