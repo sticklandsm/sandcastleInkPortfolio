9bb39585dbf78f37c39d5d1fbb9a12709d6b356e
Sean Stickland <sticklandsm@gmail.com>
2024-01-25 12:51:41 +1300

    Added animations

--- client/components/ClientRoutes.tsx
+++ client/components/ClientRoutes.tsx
@@ -1,21 +1,25 @@
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import Projects from './Projects'
 import AboutMe from './AboutMe'
 import Contact from './Contact'
 import CurriculumVitae from './CurriculumVitae'
-import { Route, Routes } from 'react-router-dom'
-import { TransitionGroup } from 'react-transition-group'
 
 export default function ClientRoutes() {
+  const location = useLocation()
+
   return (
-    <>
+    <div className="route-container">
       <TransitionGroup>
-        <Routes>
-          <Route path="/" element={<AboutMe />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/curriculumvitae" element={<CurriculumVitae />} />
-        </Routes>
+        <CSSTransition key={location.key} timeout={700} classNames="slide">
+          <Routes location={location}>
+            <Route path="/" element={<AboutMe />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/curriculumvitae" element={<CurriculumVitae />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </CSSTransition>
       </TransitionGroup>
-    </>
+    </div>
   )
 }
--- client/components/CurriculumVitae.tsx
+++ client/components/CurriculumVitae.tsx
@@ -2,26 +2,26 @@
 
 export default function CurriculumVitae() {
   return (
-    <div className="flex flex-col items-center space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-5xl mx-auto border border-white dark:border-0">
-      <a
-        href="/cv/SeanSticklandCV.pdf"
-        download
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
-      >
-        Download CV
-      </a>
-      <iframe
-        src="/cv/SeanSticklandCV.pdf"
-        width="100%"
-        height="1000px" // Adjust height as needed
-        style={{ border: 'none' }}
-        title="CurriculumVitae"
-      />
-      {/* <div className="scale-50  md:scale-100">
-        <div className="mt-0 pdf-container">
-          <PdfViewer file="/cv/SeanSticklandCV.pdf" />
-        </div>
-      </div> */}
+    <div className="py-10">
+      <div className="max-w-xs md:max-w-5xl mx-auto text-center">
+        <a
+          href="/cv/SeanSticklandCV.pdf"
+          download
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+        >
+          Download CV
+        </a>
+        <br />
+        <br />
+
+        <iframe
+          src="/cv/SeanSticklandCV.pdf"
+          className="w-full mx-auto"
+          height="1000px" // Adjust height as needed
+          style={{ border: 'none' }}
+          title="CurriculumVitae"
+        />
+      </div>
     </div>
   )
 }
